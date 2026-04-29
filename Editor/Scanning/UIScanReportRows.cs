638c1e81694993b7830d2e12ca71ec9c10453a86
luoxiaobin <314892728@qq.com>
2026-04-30 03:42:06 +0800

    Validate core scan report rows

--- Editor/Scanning/UIScanReportRows.cs
+++ Editor/Scanning/UIScanReportRows.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace Xipin.UIAITools
@@ -67,7 +68,92 @@ namespace Xipin.UIAITools
         static List<Dictionary<string, string>> ReadCore(UIAIToolsProfile profile, string report)
         {
             UIReportValidationService.ValidateReport(profile, report, UIReportFiles.CoreReportHeaders[report]);
-            return UIReportCsv.ReadRows(profile.logRoot, report);
+            var rows = UIReportCsv.ReadRows(profile.logRoot, report);
+            foreach (var row in rows)
+                ValidateRow(report, row);
+            return rows;
+        }
+
+        static void ValidateRow(string report, Dictionary<string, string> row)
+        {
+            if (report == UIReportFiles.ReuseIndex)
+            {
+                Require(report, row, "Path", "Name", "Guid", "SizeClass", "Kind", "Hash", "Advice", "Reason");
+                RequireInts(report, row, "Width", "Height", "PrefabCount", "OwnerCount", "SameHashCount");
+            }
+            else if (report == UIReportFiles.PrefabImageDetails)
+            {
+                Require(report, row, "Prefab", "Image", "Kind", "Name", "Guid", "SizeClass", "Match");
+                RequireInts(report, row, "Width", "Height");
+            }
+            else if (report == UIReportFiles.PrefabAtlasBreakdown)
+            {
+                Require(report, row, "Prefab", "Atlas", "Match", "Images");
+                RequireInts(report, row, "ImageCount");
+            }
+            else if (report == UIReportFiles.PrefabDrawCallRisk)
+            {
+                Require(report, row, "Prefab", "Owner");
+                RequireInts(report, row, "GraphicCount", "ImageCount", "RawImageCount", "OtherGraphicCount", "ImagePlusCount", "NullSpriteImageCount", "AtlasImageCount", "LooseTextureImageCount", "UniqueAtlasCount", "UniqueLooseTextureCount", "UniqueTextureCount", "UniqueMaterialCount", "EstimatedBatchGroups", "ImageBatchGroups", "TextureSwitches", "ImageTextureSwitches", "MaterialSwitches", "NestedCanvasCount", "MaskCount", "RectMask2DCount");
+            }
+            else if (report == UIReportFiles.PrefabBatchSequence)
+            {
+                Require(report, row, "Prefab", "Owner", "Path", "Type", "Source", "Kind", "Canvas", "BatchKey");
+                RequireInts(report, row, "Index");
+            }
+            else if (report == UIReportFiles.PrefabBatchBreaks)
+            {
+                Require(report, row, "Prefab", "Owner", "Path", "Reason", "Advice");
+                RequireInts(report, row, "Index");
+            }
+            else if (report == UIReportFiles.PrefabBatchBreakSummary)
+            {
+                Require(report, row, "Prefab", "Owner");
+                RequireInts(report, row, "BreakCount", "TextureBreaks", "TextBreaks", "MaterialBreaks", "CanvasBreaks", "CrossAtlasBreaks", "LooseTextureBreaks");
+            }
+            else if (report == UIReportFiles.PrefabOptimizationTargets)
+            {
+                Require(report, row, "Prefab", "Owner", "MainIssue", "NextStep");
+                RequireInts(report, row, "PriorityScore", "ImageBatchGroups", "EstimatedBatchGroups", "BreakCount", "CrossAtlasBreaks", "LooseTextureBreaks", "TextBreaks", "MaterialBreaks", "CanvasBreaks", "AtlasCount", "UITextureCount", "LargeTextureCount");
+            }
+            else if (report == UIReportFiles.PrefabNullSpriteImages)
+            {
+                Require(report, row, "Prefab", "Owner", "Path", "Node", "Advice", "Reason");
+                RequireInts(report, row, "Width", "Height");
+            }
+            else if (report == UIReportFiles.LooseTextureCandidates)
+            {
+                Require(report, row, "Path", "Name", "Guid", "SizeClass", "Advice", "Reason");
+                RequireInts(report, row, "Width", "Height", "UseCount", "PrefabCount", "OwnerCount");
+            }
+            else if (report == UIReportFiles.TextureSizeReport)
+            {
+                Require(report, row, "Path", "Name", "Guid", "Hash", "SizeClass", "Advice");
+                RequireInts(report, row, "Width", "Height", "Area", "Bytes", "Memory", "PrefabCount");
+            }
+            else if (report == UIReportFiles.AssetTriageReport)
+            {
+                Require(report, row, "Path", "Name", "Guid", "Hash", "Advice", "Reason");
+                RequireInts(report, row, "Width", "Height", "Bytes", "Memory", "NameDup");
+            }
+        }
+
+        static void Require(string report, Dictionary<string, string> row, params string[] fields)
+        {
+            foreach (var field in fields)
+            {
+                if (string.IsNullOrEmpty(row[field]))
+                    throw new Exception($"Invalid {report}: {field} is required");
+            }
+        }
+
+        static void RequireInts(string report, Dictionary<string, string> row, params string[] fields)
+        {
+            foreach (var field in fields)
+            {
+                if (!long.TryParse(row[field], out _))
+                    throw new Exception($"Invalid {report}: {field} must be an integer");
+            }
         }
     }
 }
